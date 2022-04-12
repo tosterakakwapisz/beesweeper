cd016659ed7a72c3dc2ee5d99ff1609b871ae988
TheToster <kasper.knapik@gmail.com>
2022-04-12 12:32:53 +0200

    Update sketch for more modern approach, plus clean up mess a bit

--- sketch.js
+++ sketch.js
@@ -1,78 +1,69 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Minesweeper
-// Video: https://youtu.be/LFU5ZlrR21E
+let grid;
+let cols;
+let rows;
+let w = 20;
+let totalBees = 30;
 
 function make2DArray(cols, rows) {
-  var arr = new Array(cols);
-  for (var i = 0; i < arr.length; i++) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
   }
   return arr;
 }
 
-var grid;
-var cols;
-var rows;
-var w = 20;
-
-var totalBees = 30;
 
 function setup() {
   createCanvas(401, 401);
   cols = floor(width / w);
   rows = floor(height / w);
   grid = make2DArray(cols, rows);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j] = new Cell(i, j, w);
     }
   }
 
   // Pick totalBees spots
-  var options = [];
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  let options = [];
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       options.push([i, j]);
     }
   }
 
-  for (var n = 0; n < totalBees; n++) {
-    var index = floor(random(options.length));
-    var choice = options[index];
-    var i = choice[0];
-    var j = choice[1];
+  for (let n = 0; n < totalBees; n++) {
+    let index = floor(random(options.length));
+    let choice = options[index];
+    let i = choice[0];
+    let j = choice[1];
     // Deletes that spot so it's no longer an option
     options.splice(index, 1);
     grid[i][j].bee = true;
   }
 
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j].countBees();
     }
   }
 }
 
 function gameOver() {
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j].revealed = true;
     }
   }
 }
 
 function mousePressed() {
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      if (grid[i][j].contains(mouseX, mouseY)) {
-        grid[i][j].reveal();
-
-        if (grid[i][j].bee) {
-          gameOver();
-        }
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let cell = grid[i][j];
+      if (cell.contains(mouseX, mouseY)) {
+        cell.reveal();
+        if (cell.bee) gameOver();
       }
     }
   }
@@ -80,8 +71,8 @@ function mousePressed() {
 
 function draw() {
   background(255);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j].show();
     }
   }
